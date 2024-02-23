a243c6061b9c9c811246b30795216ac4ef8971fe
ViktorAgapov <stayers.08chantry@icloud.com>
2024-02-23 13:59:20 +0300

    chaining

--- db/models/user.js
+++ db/models/user.js
@@ -2,15 +2,11 @@
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Round }) {
+      this.hasMany((Round, { foreignKey: 'id' }));
     }
   }
   User.init({
